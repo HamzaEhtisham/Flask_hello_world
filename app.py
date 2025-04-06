e9d6ad599e0ad02898b2df162e972b71a61ad3aa
HamzaEhtisham <hamzaehtisham124@gmail.com>
2025-04-06 11:38:03 +0500

    new changes

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
+# Flask example: Bind to ALL interfaces (0.0.0.0)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)  # Replace 5000 with your port
