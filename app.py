4d70f3e1f5eb42addf4257f96f8971d71844918d
HamzaEhtisham <hamzaehtisham124@gmail.com>
2025-04-08 05:29:01 +0500

    new changes-two more end points

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, my name is hamza"
 
+@app.route('/bio')
+def biodata():
+    return "age is 20 \nstudy in uit university \nlive in karachi"
+
+@app.route('/study')
+def studydetail():
+    return "name:uit univeersity \ndegree:BS(CS) "
 # Flask example: Bind to ALL interfaces (0.0.0.0)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Replace 5000 with your port
